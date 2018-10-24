1824579cca78edf05dc829c989a889724749db06
zhubenle <zhubenle@iautos.cn>
2018-10-24 14:01:49 +0800

    包调整

---
+++ src/test/java/com/t0mpi9/captcha/CaptchaClientTest.java
@@ -0,0 +1,17 @@
+package com.t0mpi9.captcha;
+
+import org.junit.Test;
+
+/**
+ * <br/>
+ * Created on 2018/10/24 13:58.
+ *
+ * @author zhubenle
+ */
+public class CaptchaClientTest {
+
+    @Test
+    public void testCaptcha() throws Exception{
+        CaptchaClient captchaClient = new CaptchaClient();
+    }
+}
